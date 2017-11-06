6458bb9acc29dcbdce490eb2a24f491837509f1e
root <root@dhcp-128-53.nay.redhat.com>
2017-11-06 11:48:33 +0800

    install others first

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -1,17 +1,5 @@
 cd ~
 
-#install git
-wget https://github.com/git/git/archive/v2.14.0.tar.gz
-tar -xzf git-2.14.0.tar.gz
-cd git-2.14.0
-make configure
-./configure --prefix=/usr
-make all doc info
-sudo make install install-doc install-html install-info
-
-#get the example
-git clone https://github.com/zwqStella/Resteasy-example-zoo
-
 #install maven
 echo "maven installing..."
 wget https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
@@ -30,3 +18,16 @@ rm -f wildfly-11.0.0.Final.tar.gz
 cd wildfly-11.0.0.Final/bin
 #./standalone.sh &
 
+cd ~
+
+#install git
+wget https://github.com/git/git/archive/v2.14.0.tar.gz
+tar -xzf git-2.14.0.tar.gz
+cd git-2.14.0
+make configure
+./configure --prefix=/usr
+make all doc info
+sudo make install install-doc install-html install-info
+
+#get the example
+git clone https://github.com/zwqStella/Resteasy-example-zoo
