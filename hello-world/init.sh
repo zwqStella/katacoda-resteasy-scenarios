8d40a3590a526741cab06a85f5a033fa372b20ce
root <root@dhcp-128-53.nay.redhat.com>
2017-11-06 14:24:25 +0800

    upload all the files

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -9,18 +9,3 @@ cd wildfly-11.0.0.Final/bin
 #./standalone.sh &
 
 cd ~
-
-#install git
-wget https://github.com/git/git/archive/v2.14.0.tar.gz
-tar -xzf v2.14.0.tar.gz
-rm -f v2.14.0.tar.gz
-cd git-2.14.0
-make configure
-./configure --prefix=/usr
-make all doc info
-sudo make install install-doc install-html install-info
-
-cd ~
-
-#get the example
-git clone https://github.com/zwqStella/Resteasy-example-zoo
