072d1b411c6d900a4e98852025808a661233bdbc
root <root@dhcp-128-53.nay.redhat.com>
2017-11-06 13:29:51 +0800

    try to fix init.sh

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -1,15 +1,1 @@
-cd ~
-
-#install git
-wget https://github.com/git/git/archive/v2.14.0.tar.gz
-tar -xzf v2.14.0.tar.gz
-cd v2.14.0
-make configure
-./configure --prefix=/usr
-make all doc info
-sudo make install install-doc install-html install-info
-
-cd ~
-
-#get the example
-git clone https://github.com/zwqStella/Resteasy-example-zoo
+echo init
