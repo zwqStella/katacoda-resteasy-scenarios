38175f798cb1f055100564651815c7c2888746a1
root <root@dhcp-128-53.nay.redhat.com>
2017-11-06 10:55:34 +0800

    change init.sh file

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -7,6 +7,7 @@ git clone https://github.com/zwqStella/Resteasy-example-zoo
 echo "maven installing..."
 wget https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
 tar -xzf apache-maven-3.5.0-bin.tar.gz
+rm -f apache-maven-3.5.0-bin.tar.gz
 export M2_HOME=~/apache-maven-3.5.0-bin
 export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
@@ -16,6 +17,7 @@ mvn -version
 echo "wildfly installing..."
 wget http://download.jboss.org/wildfly/11.0.0.Final/wildfly-11.0.0.Final.tar.gz
 tar -xzf wildfly-11.0.0.Final.tar.gz
+rm -f wildfly-11.0.0.Final.tar.gz
 cd wildfly-11.0.0.Final/bin
-./standalone.sh &
+#./standalone.sh &
 
