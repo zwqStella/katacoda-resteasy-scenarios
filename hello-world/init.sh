21212d96ad90c501cd9fc902bd7a0b616d65bc61
root <root@dhcp-128-53.nay.redhat.com>
2017-11-06 16:00:11 +0800

    delete unnecessary test files

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -1,15 +1,14 @@
-cd ~
+#prepare source code
+cd ~/tutorial
+wget https://github.com/zwqStella/Resteasy-example-zoo/raw/master/zoo.zip
+unzip zoo.zip
+rm -f zoo.zip
 
 #install wildfly
+cd ~
 echo "wildfly installing..."
 wget http://download.jboss.org/wildfly/11.0.0.Final/wildfly-11.0.0.Final.tar.gz
 tar -xzf wildfly-11.0.0.Final.tar.gz
 rm -f wildfly-11.0.0.Final.tar.gz
 cd wildfly-11.0.0.Final/bin
 #./standalone.sh &
-
-#prepare source code
-cd ~/tutorial
-wget https://github.com/zwqStella/Resteasy-example-zoo/raw/master/zoo.zip
-unzip zoo.zip
-rm -f zoo.zip
