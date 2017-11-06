f6200487ea5b199249afea2c3050f4e26812b763
root <root@dhcp-128-53.nay.redhat.com>
2017-11-06 13:41:20 +0800

    not install maven

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -1,1 +1,25 @@
-echo init
+cd ~
+
+#install wildfly
+echo "wildfly installing..."
+wget http://download.jboss.org/wildfly/11.0.0.Final/wildfly-11.0.0.Final.tar.gz
+tar -xzf wildfly-11.0.0.Final.tar.gz
+rm -f wildfly-11.0.0.Final.tar.gz
+cd wildfly-11.0.0.Final/bin
+#./standalone.sh &
+
+cd ~
+
+#install git
+wget https://github.com/git/git/archive/v2.14.0.tar.gz
+tar -xzf git-2.14.0.tar.gz
+cd git-2.14.0
+make configure
+./configure --prefix=/usr
+make all doc info
+sudo make install install-doc install-html install-info
+
+cd ~
+
+#get the example
+git clone https://github.com/zwqStella/Resteasy-example-zoo
