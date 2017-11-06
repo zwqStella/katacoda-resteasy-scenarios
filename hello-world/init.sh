e7fc0406abe9e0e33ced62796f6b63cda6a56e0a
root <root@dhcp-128-53.nay.redhat.com>
2017-11-06 11:39:47 +0800

    try another way to install git

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -1,7 +1,13 @@
 cd ~
 
 #install git
-apt-get install git
+wget https://github.com/git/git/archive/v2.14.0.tar.gz
+tar -xzf git-2.14.0.tar.gz
+cd git-2.14.0
+make configure
+./configure --prefix=/usr
+make all doc info
+sudo make install install-doc install-html install-info
 
 #get the example
 git clone https://github.com/zwqStella/Resteasy-example-zoo
