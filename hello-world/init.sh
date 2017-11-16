0a88992424bf7755fc89a4f9566ddf62679f6019
zwqStella <zwqStella@hotmail.com>
2017-11-16 17:06:34 +0800

    install maven3.5.0

--- hello-world/init.sh
+++ hello-world/init.sh
@@ -5,3 +5,12 @@ unzip -q 2.0.zip
 rm -f 2.0.zip
 mv katacoda_example_zoo-2.0/* .
 rmdir katacoda_example_zoo-2.0
+
+#install maven
+cd ~
+wget https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+tar -xzf apache-maven-3.5.0-bin.tar.gz
+rm -f apache-maven-3.5.0-bin.tar.gz
+echo 'export M2_HOME=~/apache-maven-3.5.0-bin' >> .bashrc
+echo 'export M2=$M2_HOME/bin' >> .bashrc
+echo 'export PATH=$M2:$PATH' >> .bashrc
